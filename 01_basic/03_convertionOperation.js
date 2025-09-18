19911051bf540e330a9db4cac9ea3d9684fc1b75
srikakulalaxman547 <srikakulalaxman@gmail.com>
2025-09-18 11:50:21 +0000

    convertionOperation

---
+++ 01_basic/03_convertionOperation.js
@@ -0,0 +1,15 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//null=>0
+//undefined=>Nan
+//true=>1 false=>0
+
